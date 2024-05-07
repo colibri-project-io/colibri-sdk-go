23ba1359be989f7906fca8af2975e3c1283af9e8
Rafael Ramos <arkanjo.ms@gmail.com>
2024-05-07 20:27:07 -0300

    http server

--- pkg/web/restserver/http_server.go
+++ pkg/web/restserver/http_server.go
@@ -1,41 +1,49 @@
 package restserver
 
-import "net/http"
+import (
+	"context"
+	"fmt"
+	"github.com/colibri-project-io/colibri-sdk-go/pkg/base/config"
+	"github.com/colibri-project-io/colibri-sdk-go/pkg/base/logging"
+	"net/http"
+)
 
 type httpWebServer struct {
 	srv *http.Server
 }
 
 func newHttpWebServer() Server {
-	return &httpWebServer{}
+	return &httpWebServer{
+		srv: &http.Server{
+			Addr:    fmt.Sprintf(":%d", config.PORT),
+			Handler: http.NewServeMux(),
+		},
+	}
 }
 
-func (srv *httpWebServer) initialize() {
+func (h *httpWebServer) initialize() {
+	logging.Debug("Initializing HTTP Server at port ")
+}
+
+func (h *httpWebServer) shutdown() error {
+	return h.srv.Shutdown(context.Background())
+}
+
+func (h *httpWebServer) injectMiddlewares() {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (srv *httpWebServer) shutdown() error {
+func (h *httpWebServer) injectCustomMiddlewares() {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (srv *httpWebServer) injectMiddlewares() {
+func (h *httpWebServer) injectRoutes() {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (srv *httpWebServer) injectCustomMiddlewares() {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (srv *httpWebServer) injectRoutes() {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (srv *httpWebServer) listenAndServe() error {
-	//TODO implement me
-	panic("implement me")
+func (h *httpWebServer) listenAndServe() error {
+	return h.srv.ListenAndServe()
 }
