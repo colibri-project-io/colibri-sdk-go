62aa74c2d637e957b333474dc09c2315ec3f029f
Rafael Dantas <dantasrafael@yahoo.com.br>
2024-05-13 23:01:39 -0300

    feat: isodate and isotime tests

--- pkg/base/types/iso_date_test.go
+++ pkg/base/types/iso_date_test.go
@@ -8,6 +8,42 @@ import (
 )
 
 func TestIsoDate(t *testing.T) {
+	t.Run("Should get parsed iso date", func(t *testing.T) {
+		expected := IsoDate(time.Date(2022, time.January, 30, 0, 0, 0, 0, time.UTC))
+
+		result, err := ParseIsoDate("2022-01-30")
+
+		assert.Nil(t, err)
+		assert.NotNil(t, result)
+		assert.Equal(t, expected, result)
+	})
+
+	t.Run("Should return error when parse with a invalid string", func(t *testing.T) {
+		result, err := ParseIsoDate("invalid")
+
+		assert.NotNil(t, err)
+		assert.NotNil(t, result)
+		assert.Equal(t, IsoDate(time.Date(1, time.January, 1, 0, 0, 0, 0, time.UTC)), result)
+	})
+
+	t.Run("Should get string iso date", func(t *testing.T) {
+		expected := "2022-01-30"
+
+		result := IsoDate(time.Date(2022, time.January, 30, 0, 0, 0, 0, time.UTC)).String()
+
+		assert.NotNil(t, result)
+		assert.Equal(t, expected, result)
+	})
+
+	t.Run("Should get go string iso date", func(t *testing.T) {
+		expected := "time.Date(2022, time.January, 30, 0, 0, 0, 0, time.UTC)"
+
+		result := IsoDate(time.Date(2022, time.January, 30, 0, 0, 0, 0, time.UTC)).GoString()
+
+		assert.NotNil(t, result)
+		assert.Equal(t, expected, result)
+	})
+
 	t.Run("Should get value with a valid value", func(t *testing.T) {
 		expected := time.Date(2022, time.January, 1, 0, 0, 0, 0, time.UTC)
 
--- pkg/base/types/iso_time_test.go
+++ pkg/base/types/iso_time_test.go
@@ -8,6 +8,42 @@ import (
 )
 
 func TestIsoTime(t *testing.T) {
+	t.Run("Should get parsed iso time", func(t *testing.T) {
+		expected := IsoTime(time.Date(0, time.January, 1, 10, 20, 30, 0, time.UTC))
+
+		result, err := ParseIsoTime("10:20:30")
+
+		assert.Nil(t, err)
+		assert.NotNil(t, result)
+		assert.Equal(t, expected, result)
+	})
+
+	t.Run("Should return error when parse with a invalid string", func(t *testing.T) {
+		result, err := ParseIsoTime("invalid")
+
+		assert.NotNil(t, err)
+		assert.NotNil(t, result)
+		assert.Equal(t, IsoTime(time.Date(1, time.January, 1, 0, 0, 0, 0, time.UTC)), result)
+	})
+
+	t.Run("Should get string iso time", func(t *testing.T) {
+		expected := "10:20:30"
+
+		result := IsoTime(time.Date(0, time.January, 1, 10, 20, 30, 0, time.UTC)).String()
+
+		assert.NotNil(t, result)
+		assert.Equal(t, expected, result)
+	})
+
+	t.Run("Should get go string iso time", func(t *testing.T) {
+		expected := "time.Date(0, time.January, 1, 10, 20, 30, 0, time.UTC)"
+
+		result := IsoTime(time.Date(0, time.January, 1, 10, 20, 30, 0, time.UTC)).GoString()
+
+		assert.NotNil(t, result)
+		assert.Equal(t, expected, result)
+	})
+
 	t.Run("Should get value with a valid value", func(t *testing.T) {
 		expected := time.Date(1, time.January, 1, 10, 20, 30, 0, time.UTC)
 
