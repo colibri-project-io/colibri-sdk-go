91372e4a6a6b2e06cf0ebe5404fb68f2550cd0be
Rafael Dantas <36964299+dantasrafael@users.noreply.github.com>
2024-08-09 21:58:45 -0300

    feat: add sort-direction validator (#103)

--- pkg/base/types/sort.go
+++ pkg/base/types/sort.go
@@ -8,6 +8,19 @@ const (
 	DESC SortDirection = "DESC"
 )
 
+// sortDirections is a map that represents the valid sort directions.
+// It maps a SortDirection value to a boolean indicating whether the direction is valid.
+var sortDirections = map[SortDirection]bool{
+	ASC:  true,
+	DESC: true,
+}
+
+// IsValid checks if the SortDirection is valid.
+// It returns true if the SortDirection is valid, otherwise false.
+func (sd SortDirection) IsValid() bool {
+	return sortDirections[sd]
+}
+
 // Sort is the contract to sort an field
 type Sort struct {
 	Direction SortDirection
